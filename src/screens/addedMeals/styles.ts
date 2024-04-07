aacdf61a25aaa453e9ea05d34f9a24ffd6f0da07
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-04-07 20:12:53 -0300

    Create styles.ts

---
+++ src/screens/addedMeals/styles.ts
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin-top: 20px;
+  margin-left: 24px;
+`
+
+export const Card = styled.View`
+  width: 300px;
+  flex-direction: row;
+  justify-content: space-between;
+  background: #ffffff;
+  padding: 10px;
+  border-radius: 10px;
+  margin-top: 24px;
+`
+
+export const Info = styled.View`
+  background: #957FEF;
+  padding: 10px;
+  border-radius: 10px;
+  text-align: center;
+  margin-top: 16px;
+`
