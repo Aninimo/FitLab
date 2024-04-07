c68d324ef2da46ff46bb49a8d7b1580df3409e95
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-04-07 18:56:05 -0300

    Create styles.ts

---
+++ src/components/input/styles.ts
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components/native';
+
+export const InputContainer = styled.TextInput`
+ border: 1px solid #ADB5BD;
+ border-radius: 10px;
+ padding: 10px;
+ margin-bottom: 30px;
+`
