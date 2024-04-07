c806edcbfb0f0f3652f640e8f19506214585ca6e
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-04-07 20:51:03 -0300

    Create styles.ts

---
+++ src/screens/login/styles.ts
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  justify-content: center;
+  padding: 10px;
+`
+
+export const Title = styled.Text`
+  font-size: 32px;
+  font-weight: bold;
+  color: #957FEF;
+  margin-top: 60px;
+  margin-bottom: 40px;
+  text-align: center;
+`
