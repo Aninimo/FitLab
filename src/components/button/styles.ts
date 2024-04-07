929ba04970c9fe393e188f965bfdf0059a3abd10
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-04-07 18:54:35 -0300

    Create styles.ts

---
+++ src/components/button/styles.ts
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+ background: #957FEF;
+ color: #ffffff;
+ padding: 10px;
+ border-radius: 10px;
+ text-align: center;
+ margin-bottom: 20px;
+`
