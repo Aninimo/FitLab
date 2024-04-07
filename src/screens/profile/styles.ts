213d5f9ccd32e4f05b8e2e2b91779c3b0cd6730c
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-04-07 20:30:59 -0300

    Create styles.ts

---
+++ src/screens/profile/styles.ts
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  padding: 20px;
+`
+
+export const Title = styled.Text`
+  font-weight: bold;
+  font-size: 24px;
+  margin-bottom: 30px;
+`
+
+export const InfoUser = styled.Text`
+  margin-bottom: 15px;
+`
